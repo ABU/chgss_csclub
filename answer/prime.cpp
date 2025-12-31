3fba1f56f3ac9d076453ff383770cb2452a85f36
unknown <ppi3106@gmail.com>
2025-12-31 12:36:47 +0800

    modify prime

--- answer/prime.cpp
+++ answer/prime.cpp
@@ -12,7 +12,6 @@ bool isPrime(int x) {
     return true;
 }
 
-
 int main() {
     int N;
     cin >> N;
@@ -22,23 +21,44 @@ int main() {
         return 0;
     }
 
+    // 情況 1：N 本身是質數
     if (isPrime(N)) {
-        cout << 1;
+        cout << 1 << endl;
+        cout << N;
         return 0;
     }
 
+    // 情況 2：N 是偶數
     if (N % 2 == 0) {
-        cout << 2;
+        for (int p = 2; p <= N; p++) {
+            if (isPrime(p) && isPrime(N - p)) {
+                cout << 2 << endl;
+                cout << p << " " << (N - p);
+                return 0;
+            }
+        }
+    }
+
+    // 情況 3：N 是奇數
+    if (isPrime(N - 2)) {
+        cout << 2 << endl;
+        cout << 2 << " " << (N - 2);
         return 0;
     }
 
-    // N 是奇數
-    if (isPrime(N - 2)) {
-        cout << 2;
-    } else {
-        cout << 3;
+    // 情況 4：一定用 3 個質數
+    // 3 + p + q = N
+    int rest = N - 3;
+    for (int p = 2; p <= rest; p++) {
+        if (isPrime(p) && isPrime(rest - p)) {
+            cout << 3 << endl;
+            cout << 3 << " " << p << " " << (rest - p);
+            return 0;
+        }
     }
 
+    // 理論上不會到這
+    cout << "I got one million dollars.";
     return 0;
 }
 
